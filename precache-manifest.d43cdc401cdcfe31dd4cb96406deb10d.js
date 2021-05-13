a129c15d2705bb09e690998d4debe7b47d66bc4d
iokan <kopytovalev@gmail.com>
2021-05-13 19:36:22 +0300

    new portfolio (start)

---
+++ precache-manifest.d43cdc401cdcfe31dd4cb96406deb10d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d60ab3289ff2fb9e3418",
+    "url": "/css/app.d294f910.css"
+  },
+  {
+    "revision": "4b8b401a99c49f409edd340a1721c8e5",
+    "url": "/fonts/Circe-Bold.4b8b401a.woff"
+  },
+  {
+    "revision": "7205e0cf1ffcb4cafccbae0e81e09912",
+    "url": "/fonts/Circe-Light.7205e0cf.woff"
+  },
+  {
+    "revision": "fa54724c5f76114b7dd0234182ca2064",
+    "url": "/fonts/Circe-Regular.fa54724c.woff"
+  },
+  {
+    "revision": "3cb2e1e0e21b79d36290b5d72c0f4a0c",
+    "url": "/img/intro_1.3cb2e1e0.jpg"
+  },
+  {
+    "revision": "124269384230c6f8555b6765e3ed828a",
+    "url": "/img/intro_2.12426938.jpg"
+  },
+  {
+    "revision": "89c362b160a746e84b38fdffb5889272",
+    "url": "/img/intro_3.89c362b1.jpg"
+  },
+  {
+    "revision": "c6aaf2efc7df7d405dfee367ddfa8b17",
+    "url": "/img/intro_4_1.c6aaf2ef.jpg"
+  },
+  {
+    "revision": "8d487a1992577204099f1ced4a53e434",
+    "url": "/img/intro_5.8d487a19.jpg"
+  },
+  {
+    "revision": "1576c003199f062e4120ac5d31cebd27",
+    "url": "/img/intro_6.1576c003.jpg"
+  },
+  {
+    "revision": "a9db55714b8fe45eea87e9763e72b8ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d60ab3289ff2fb9e3418",
+    "url": "/js/app.2f6b256d.js"
+  },
+  {
+    "revision": "b8439abec3cd990dc943",
+    "url": "/js/chunk-vendors.07457c15.js"
+  },
+  {
+    "revision": "3215e9070d84e369f3955e22d9613c93",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
